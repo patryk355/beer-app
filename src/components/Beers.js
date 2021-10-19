d368d5a584e4d3641fef103a2e623d56b84a3590
Patryk Karasiński <patrykkarasinski11@gmail.com>
2021-10-19 20:30:54 +0200

    add search input

--- src/components/Beers.js
+++ src/components/Beers.js
@@ -5,6 +5,7 @@ import { useHistory } from 'react-router-dom';
 const Beers = () => {
     const [beers, setBeers] = useState(null);
     const [fav, setFav] = useState(null);
+    const [searchTerm, setSearchTerm] = useState("");
     let history = useHistory();
 
 
@@ -12,7 +13,6 @@ const Beers = () => {
         fetch('https://api.punkapi.com/v2/beers')
             .then(res => res.json())
             .then(data => {
-                console.log(data);
                 setBeers(data);
             })
             .catch(err => console.log(err));
@@ -29,27 +29,40 @@ const Beers = () => {
         if (fav[id]) {
             fav[id] = false;
         } else {
-            // setFav(true);
             fav[id] = true;
         }
     }
 
+    const handleInput = (e) => {
+        setSearchTerm(e.target.value.toLowerCase());
+    }
+
+    const results = !searchTerm ?
+        beers
+        :
+        beers.filter(beer => beer.name.toLowerCase().includes(searchTerm));
+
     return (
         <div className="beers">
             <div className="container">
-                {beers && beers.map((beer) => (
-                    <div className="beer" key={beer.id} >
-                        <img className="beer-img" src={beer.image_url} alt={beer.name} onClick={(id) => handleClick(beer.id)} />
-                        {/* {fav[beer.id] ?
+                <div className="search">
+                    <input type="text" className="search-input" placeholder="Search..." onChange={(e) => handleInput(e)} value={searchTerm} />
+                </div>
+                <div className="beers-container">
+                    {results && results.map((beer) => (
+                        <div className="beer" key={beer.id} >
+                            <img className="beer-img" src={beer.image_url} alt={beer.name} onClick={(id) => handleClick(beer.id)} />
+                            {/* {fav[beer.id] ?
                             (<AiFillStar className="icon" onClick={(e, id) => toggleFav(e, beer.id)} />)
                             :
                             (<AiOutlineStar className="icon" onClick={(e, id) => toggleFav(e, beer.id)} />)} */}
-                        {/* <AiOutlineStar className="icon" onClick={toggleFav} /> */}
-                        {/* <AiFillStar className="icon"/> */}
-                        <h3 className="beer-name" onClick={(id) => handleClick(beer.id)}>{beer.name}</h3>
-                        <p className="beer-description" onClick={(id) => handleClick(beer.id)}>{beer.description.split('', 100)}...</p>
-                    </div>
-                ))}
+                            {/* <AiOutlineStar className="icon" onClick={toggleFav} /> */}
+                            {/* <AiFillStar className="icon"/> */}
+                            <h3 className="beer-name" onClick={(id) => handleClick(beer.id)}>{beer.name}</h3>
+                            <p className="beer-description" onClick={(id) => handleClick(beer.id)}>{beer.description.split('', 100)}...</p>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div >
     )
