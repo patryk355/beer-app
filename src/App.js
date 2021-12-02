056c4152eac515aeaac0a8a718aba74b289d02b7
Patryk Karasiński <patrykkarasinski11@gmail.com>
2021-12-02 22:21:55 +0100

    add useeffect

--- src/App.js
+++ src/App.js
@@ -3,8 +3,26 @@ import Navbar from './components/Navbar';
 import Beers from './components/Beers';
 import Beer from './components/Beer';
 import Footer from './components/Footer';
+import NotFound from './components/NotFound';
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { fetchFavItems, putItemsToFav } from './store/fetch-actions';
+import { useDispatch } from 'react-redux';
+import FavouriteBeers from './components/FavouriteBeers';
 
 function App() {
+  const dispatch = useDispatch();
+
+  const favItems = useSelector(state => state.fav.favItems);
+
+  useEffect(() => {
+    dispatch(putItemsToFav(favItems));
+  }, [favItems, dispatch]);
+
+  useEffect(() => {
+    dispatch(fetchFavItems());
+  }, [dispatch]);
+
   return (
     <Router>
       <div className="app">
@@ -16,6 +34,12 @@ function App() {
           </Route>
           <Route path="/beer-app/beers/:id">
             <Beer />
+          </Route>
+          <Route path="/beer-app/favourite">
+            <FavouriteBeers />
+          </Route>
+          <Route path="*">
+            <NotFound />
           </Route>
         </Switch>
 
